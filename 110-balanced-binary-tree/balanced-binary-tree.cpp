4fde1010a808a198f023fe48a048d0096a6702fa
Abhishek kashyap <inabhishek14@gmail.com>
2026-06-18 00:37:55 +0530

    Time: 3 ms (10.18%) | Memory: 22.9 MB (82.76%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -1,33 +1,29 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-public:
-    bool isBalanced(TreeNode* root) {
-        if(root == nullptr) return true;
+private:
+    pair<bool, int> isBinaryTreeBalanced(TreeNode* root){
 
-        int leftHeight = getHeight(root->left);
-        int rightHeight = getHeight(root->right);
+        if(root == NULL){
+            return {true, 0};
+        }
 
-        if(abs(leftHeight - rightHeight) <= 1 && isBalanced(root->left) && isBalanced(root->right)) return true;
+        auto left = isBinaryTreeBalanced(root->left);
+        auto right = isBinaryTreeBalanced(root->right);
 
-        return false;
+        bool leftBalanced = left.first;
+        bool rightBalanced = right.first;
+
+        bool diff = abs(left.second - right.second) <= 1;
+
+        pair<bool, int> res;
+
+        res.first = leftBalanced && rightBalanced && diff;
+        res.second = max(left.second, right.second) + 1;
+
+        return res;
     }
 
-    int getHeight(TreeNode* root){
-        if(root == nullptr) return 0;
-
-        int leftHeight = getHeight(root->left);
-        int rightHeight = getHeight(root->right);
-
-        return max(leftHeight, rightHeight) + 1;
+public:
+    bool isBalanced(TreeNode* root) {
+        return isBinaryTreeBalanced(root).first;
     }
 };
