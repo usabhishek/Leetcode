9d05cc4e8f3989056f0af26260daf1e86af88a39
Abhishek kashyap <inabhishek14@gmail.com>
2026-08-11 00:14:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (5.36%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        if(s.size() == t.size()) return s == t;
+        int n = t.length();
+        
+        unordered_map<char, vector<int>> mp;
 
-        int i=0, j=0;
-        while(i < s.size() && j < t.size()){
-            if(s[i] == t[j]){
-                i++;
-                j++;
-            }else{
-                j++;
-            }
+        for (int i = 0; i < n; i++) {
+            mp[t[i]].push_back(i);
         }
 
-        return i == s.size();
+        int j = -1;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s[i];
+
+            if (mp.find(ch) != mp.end()) {
+
+                vector<int> vec = mp[ch];
+
+                auto it = upper_bound(vec.begin(), vec.end(), j);
+
+                if (it != vec.end()) j = *it;
+                else return false;
+            } 
+            else return false;
+        }
+
+        return true;
     }
 };
