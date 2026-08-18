7ce4eaf5ae81f81d49813fe61878ecf10abafc8a
Abhishek kashyap <inabhishek14@gmail.com>
2026-08-19 00:11:38 +0530

    Time: 0 ms (100.00%) | Memory: 29 MB (78.95%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.cpp
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.cpp
@@ -3,50 +3,22 @@ public:
     int largestInteger(vector<int>& nums, int k) {
         int n = nums.size();
 
+        vector<int> freq(51, 0);
+        for (int x : nums) freq[x]++;
+
+        if (k == n) return *max_element(nums.begin(), nums.end());
+
         if (k == 1) {
-            unordered_map<int, int> freq;
-
-            for (int x : nums) {
-                freq[x]++;
-            }
-
             int ans = -1;
 
-            for (auto& [x, cnt] : freq) {
-                if (cnt == 1) {
-                    ans = max(ans, x);
-                }
-            }
-
+            for (int x : nums) if (freq[x] == 1) ans = max(ans, x);
             return ans;
-        }
-
-        if (k == n) {
-            return *max_element(nums.begin(), nums.end());
-        }
-
-        int first = nums[0];
-        int last = nums[n - 1];
-
-        int cntFirst = 0;
-        int cntLast = 0;
-
-        for (int x : nums) {
-            if (x == first)
-                cntFirst++;
-
-            if (x == last)
-                cntLast++;
         }
 
         int ans = -1;
 
-        if (cntFirst == 1)
-            ans = max(ans, first);
-
-        if (cntLast == 1)
-            ans = max(ans, last);
-
+        if (freq[nums[0]] == 1) ans = max(ans, nums[0]);
+        if (freq[nums[n - 1]] == 1) ans = max(ans, nums[n - 1]);
         return ans;
     }
 };
